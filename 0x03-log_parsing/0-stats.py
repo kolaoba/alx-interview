cb20393b9b62b8d020b0c673ef70efb70b3393d3
Kolapo Obajulwa <Kolaobajulwa@gmail.com>
2023-04-13 20:33:21 +0100

    refactor 0-stats

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -14,14 +14,20 @@ def parse_logs():
     global file_size
     global status_dict
     file_size = 0
-    status_dict = {}
+    status_dict = {200: 0,
+                   301: 0,
+                   400: 0,
+                   401: 0,
+                   403: 0,
+                   404: 0,
+                   405: 0,
+                   500: 0}
 
     for line in sys.stdin:
         logs = line.split(" ")
-        if int(logs[7]) not in status_dict.keys():
-            status_dict[int(logs[7])] = 0
-        file_size += int(logs[-1])
-        status_dict[int(logs[7])] += 1
+        if int(logs[-2]) in status_dict.keys() and len(logs) == 9:
+            file_size += int(logs[-1])
+            status_dict[int(logs[-2])] += 1
         if count % 10 == 0 and count != 0:
             print_output(status_dict, file_size)
         count += 1
@@ -37,7 +43,8 @@ def print_output(status_dict, file_size):
     print("File size: {}".format(file_size))
     od = OrderedDict(sorted(status_dict.items()))
     for k, v in od.items():
-        print("{}: {}".format(k, v))
+        if v > 0:
+            print("{}: {}".format(k, v))
 
 
 if __name__ == '__main__':
