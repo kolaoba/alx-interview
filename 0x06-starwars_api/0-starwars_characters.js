c86dd6912cacef5e62f5eeb93c7c9468e01786d1
Kolapo Obajulwa <Kolaobajulwa@gmail.com>
2023-05-06 13:35:20 +0100

    add lint - semistandard

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -8,29 +8,29 @@ const args = process.argv.slice(1);
 if (args.length < 2) {
   console.log('Error');
   process.exit(1);
-};
+}
 
 const movieId = args[1];
-  request(`${url}${movieId}`, (err, res, body) => {
-    if (err) {
-      console.error(err);
-    } else {
-      const data = JSON.parse(body);
-      const characters = data.characters;
-      const characterNames = characters.map(characterUrl => {
-        return new Promise((resolve, reject) => {
-          request(characterUrl, (err, res, body) => {
-            if (err) {
-              reject(err);
-            } else {
-              const characterData = JSON.parse(body);
-              resolve(characterData.name);
-            }
-          });
+request(`${url}${movieId}`, (err, res, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    const data = JSON.parse(body);
+    const characters = data.characters;
+    const characterNames = characters.map(characterUrl => {
+      return new Promise((resolve, reject) => {
+        request(characterUrl, (err, res, body) => {
+          if (err) {
+            reject(err);
+          } else {
+            const characterData = JSON.parse(body);
+            resolve(characterData.name);
+          }
         });
       });
-      Promise.all(characterNames).then(names => {
-        names.forEach(name => console.log(name));
-      });
-    }
-  });
+    });
+    Promise.all(characterNames).then(names => {
+      names.forEach(name => console.log(name));
+    });
+  }
+});
