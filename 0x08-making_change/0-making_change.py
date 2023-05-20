705aa8d95d5ee575eca84102d9654f527dc43d36
Kolapo Obajulwa <Kolaobajulwa@gmail.com>
2023-05-20 18:03:50 +0100

    add lint

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """
-Given a pile of coins of different values, determine the fewest 
+Given a pile of coins of different values, determine the fewest
 number of coins needed to meet a given amount total.
 """
 
-from typing import List
 
-def makeChange(coins: List[int], total: int) -> int:
+def makeChange(coins, total) -> int:
     """
     Returns the fewest number of coins
     needed to meet a given amount total
