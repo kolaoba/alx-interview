49fe8abc2bb27376aa42c5544686c0e69f3d4b9c
Kolapo Obajulwa <Kolaobajulwa@gmail.com>
2023-05-20 18:01:00 +0100

    add task 0

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Given a pile of coins of different values, determine the fewest 
+number of coins needed to meet a given amount total.
+"""
+
+
+def makeChange(coins, total):
+    """
+    Returns the fewest number of coins
+    needed to meet a given amount total
+    """
+    counter = 0
+    if total <= 0:
+        return 0
+    sort = sorted(coins, reverse=True)
+    for i in range(len(sort)):
+        while sort[i] <= total:
+            total = total - sort[i]
+            counter += 1
+    if total == 0:
+        return counter
+    return -1
